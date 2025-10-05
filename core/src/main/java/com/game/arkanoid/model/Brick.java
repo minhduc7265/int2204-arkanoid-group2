de0c8fcb02fcdf04c76edb1ddbc2e6e4c17cb0be
LE Tuan Anh <tuananhlee8888@gmail.com>
2025-10-05 19:59:09 +0700

    thêm StrongBrick, NormalBrick, Brick

--- core/src/main/java/com/game/arkanoid/model/Brick.java
+++ core/src/main/java/com/game/arkanoid/model/Brick.java
@@ -1,4 +1,27 @@
 package com.game.arkanoid.model;
-
-public class Brick extends GameObject {
+public abstract class Brick extends GameObject {
+    protected int hitPoints;
+    protected boolean destroyed;
+    public Brick(float x, float y, int width, int height, int hitPoints, String textureID) {
+        super(x, y, width, height, textureID);
+        this.hitPoints = hitPoints;
+        this.destroyed = false;
+    }
+    public void hit() {
+        if (!destroyed) {
+            hitPoints--;
+            if (hitPoints <= 0) {
+                destroyed = true;
+                onDestroyed();
+            }
+        }
+    }
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+    protected void onDestroyed() {
+    }
+    public abstract void update();
 }
+
+
--- core/src/main/java/com/game/arkanoid/model/StrongBrick.java
+++ core/src/main/java/com/game/arkanoid/model/StrongBrick.java
@@ -1,4 +1,16 @@
 package com.game.arkanoid.model;
-
 public class StrongBrick extends Brick {
+    public StrongBrick(float x, float y, int width, int height, String textureID) {
+        super(x, y, width, height, 2, textureID);
+    }
+    @Override
+    public void update() {
+        if (hitPoints == 1) {
+        }
+    }
+    @Override
+    protected void onDestroyed() {
+        System.out.println("Strong brick destroyed!");
+    }
 }
+
