43315f0e04655c0a27897a7014c033b783bceec5
LE Tuan Anh <tuananhlee8888@gmail.com>
2025-10-11 21:38:18 +0700

    class Brick

--- core/src/main/java/com/game/arkanoid/model/StrongBrick.java
+++ core/src/main/java/com/game/arkanoid/model/StrongBrick.java
@@ -1,4 +1,22 @@
 package com.game.arkanoid.model;
 
+
 public class StrongBrick extends Brick {
+
+    public StrongBrick(float x, float y, int width, int height, String textureID, int hitPoints) {
+        super(x, y, width, height, hitPoints, textureID);
+    }
+
+    @Override
+    public void update() {
+        if (hitPoints == 1) {
+            setTextureID("strong_brick_damaged");
+        }
+    }
+
+    @Override
+    protected void onDestroyed() {
+        System.out.println("Strong brick destroyed!");
+    }
 }
+
