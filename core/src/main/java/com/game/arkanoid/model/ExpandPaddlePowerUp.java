b8ce4cea30fd5eb146e7f1dade5f569ddc5ae35a
Duy123343 <hpduy.tb@gmail.com>
2025-10-12 09:55:47 +0700

    Update ExpandPaddlePowerUp.java

--- core/src/main/java/com/game/arkanoid/model/ExpandPaddlePowerUp.java
+++ core/src/main/java/com/game/arkanoid/model/ExpandPaddlePowerUp.java
@@ -1,4 +1,47 @@
 package com.game.arkanoid.model;
 
-public class ExpandPaddlePowerUp extends PowerUp {
+public class ExpandPaddlePowerUp extends PowerUp { 
+    private static final int EXPAND_AMOUNT = 30;
+    private static final int DEFAULT_DURATION = 600;
+
+    /**
+     * Khởi tạo ExpandPaddlePowerUp.
+     * @param x Tọa độ x ban đầu.
+     * @param y Tọa độ y ban đầu.
+     */
+    public ExpandPaddlePowerUp(float x, float y) {
+        super(x, y, "Expand", DEFAULT_DURATION);
+    }
+
+    /**
+     * Áp dụng hiệu ứng: Tăng chiều rộng hiện tại của Paddle.
+     * <p>Nó cũng lưu lại kích thước gốc của Paddle trước khi áp dụng hiệu ứng.</p>
+     * * @param context Đối tượng GameContext.
+     */
+    @Override
+    public void applyEffect(GameContext context) {
+        Paddle paddle = context.getPaddle();
+
+        if (!isPickedUp()) {
+            paddle.setOriginalWidth(paddle.getCurrentWidth());
+        }
+
+        paddle.setCurrentWidth(paddle.getCurrentWidth() + EXPAND_AMOUNT);
+        setPickedUp(true);
+        System.out.println(paddle.getCurrentWidth());
+    }
+
+    /**
+     * Hoàn tác hiệu ứng: Đặt chiều rộng Paddle trở lại kích thước gốc đã lưu.
+     * * @param context Đối tượng GameContext.
+     */
+    @Override
+    public void removeEffect(GameContext context) {
+        Paddle paddle = context.getPaddle();
+
+        paddle.setCurrentWidth(paddle.getOriginalWidth());
+        System.out.println(paddle.getCurrentWidth());
+    }
 }
+
+
