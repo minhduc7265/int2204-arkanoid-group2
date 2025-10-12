bf2fdcc3442a0eb3c011784a0b9e710e665f17b9
Duy123343 <hpduy.tb@gmail.com>
2025-10-12 09:56:57 +0700

    Update FastBallPowerUp.java

--- core/src/main/java/com/game/arkanoid/model/FastBallPowerUp.java
+++ core/src/main/java/com/game/arkanoid/model/FastBallPowerUp.java
@@ -1,4 +1,47 @@
 package com.game.arkanoid.model;
 
-public class FastBallPowerUp extends PowerUp {
+public class FastBallPowerUp extends PowerUp { 
+     private static final float SPEED_MULTIPLIER = 1.5f;
+    private static final int DEFAULT_DURATION = 480;
+
+    /**
+     * Khởi tạo FastBallPowerUp.
+     * @param x Tọa độ x ban đầu.
+     * @param y Tọa độ y ban đầu.
+     */
+    public FastBallPowerUp(float x, float y) {
+        super(x, y, "Fast", DEFAULT_DURATION);
+    }
+
+    /**
+     * Áp dụng hiệu ứng: Tăng tốc độ hiện tại của Ball mà vẫn giữ nguyên hướng.
+     * <p>Nó cũng lưu lại tốc độ gốc của Ball trước khi áp dụng hiệu ứng.</p>
+     * * @param context Đối tượng GameContext.
+     */
+    @Override
+    public void applyEffect(GameContext context) {
+        Ball ball = context.getBall();
+
+        if (!isPickedUp()) {
+            ball.setOriginalSpeed(ball.getCurrentSpeed());
+        }
+
+        float newSpeed = ball.getCurrentSpeed() * SPEED_MULTIPLIER;
+        ball.applySpeed(newSpeed);
+
+        setPickedUp(true);
+        System.out.println(ball.getCurrentSpeed());
+    }
+
+    /**
+     * Hoàn tác hiệu ứng: Đặt tốc độ Ball trở lại tốc độ gốc đã lưu.
+     * * @param context Đối tượng GameContext.
+     */
+    @Override
+    public void removeEffect(GameContext context) {
+        Ball ball = context.getBall();
+
+        ball.applySpeed(ball.getOriginalSpeed());
+        System.out.println(ball.getCurrentSpeed());
+    }
 }
